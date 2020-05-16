9559f9cc30b3b5caf32429a338576878bb2bdcb8
kdcheng10 <kdcheng10@gmail.com>
2020-05-16 14:42:52 -0500

    basic commands

--- src/app.js
+++ src/app.js
@@ -2,19 +2,6 @@ const Discord = require('discord.js');
 const { AkairoClient } = require('discord-akairo');
 const DBL = require('dblapi.js');
 const constants = require('./constants');
-const express = require('express');
-const http = require('http');
-
-const app = express();
-const server = http.createServer(app);
-
-app.get('/', (req, res) => {
-  res.send('ping');
-});
-
-server.listen(process.env.PORT, () => {
-  console.log(`Listening on ${process.env.PORT}`);
-});
 
 const client = new AkairoClient({
   prefix: constants.prefix,
