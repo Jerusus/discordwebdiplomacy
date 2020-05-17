86da75c42464279008df111c9cee8116f39e2f14
kdcheng10 <kdcheng10@gmail.com>
2020-05-17 12:27:15 -0500

    cookie config

--- src/commands/cookie.js
+++ src/commands/cookie.js
@@ -20,6 +20,37 @@ class CookieCommand extends Command {
   exec(message) {
     var userId = message.author.id;
     console.log(`Called cookie by ${userId}`);
+    var instructions = `instructions`; //todo
+    message.channel.send(instructions);
+    var codePrompt = `codePrompt`; //todo
+    message.channel.send(codePrompt);
+    const codeCollector = message.channel.createMessageCollector(
+      (m) => m.author.id == userId,
+      {
+        max: 1,
+      }
+    );
+    codeCollector.on('collect', (m) => {
+      if (m == 'quit' || m == constants.prefix + 'quit') {
+      } else {
+        message.channel.send('collected: ' + m);
+        var keyPrompt = `keyPrompt`; //todo
+        message.channel.send(keyPrompt);
+        const keyCollector = message.channel.createMessageCollector(
+          (m) => m.author.id == userId,
+          {
+            max: 1,
+          }
+        );
+        keyCollector.on('collect', (m) => {
+          if (m == 'quit' || m == constants.prefix + 'quit') {
+          } else {
+            message.channel.send('collected: ' + m);
+            message.channel.send('I can do stuff now');
+          }
+        });
+      }
+    });
   }
 }
 
